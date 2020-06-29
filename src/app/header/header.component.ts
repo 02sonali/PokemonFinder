68f89da1e728bf8acdd70269e4e8830689087921
unknown <02sonali@gmail.com>
2020-06-29 19:11:23 +0530

    routing fix for product details

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  id:string = '1';
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
   }
 
 }
